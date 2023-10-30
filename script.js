592e5e24135306a299b1fbb1bec8995073b80c82
Khan Faiz <khan.faiz.ayub@gmail.com>
2023-10-31 02:43:27 +0530

    Creating and inserting elements

--- script.js
+++ script.js
@@ -55,3 +55,18 @@ console.log(allButtons);
 
 console.log(document.getElementsByClassName('btn'));
 
+// Creating and inserting elements
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.textContent =
+  'We use cookie for improvemed functionality and analytics';
+// message.innerHTML =
+//   'We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+// header.prepend(message);
+header.append(message);
+// header.prepend(message.cloneNode(true));
+
+header.before(message);
+header.after(message);
